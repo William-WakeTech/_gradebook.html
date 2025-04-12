51939a0af7e8c51d2aa1415e20169c78bd902590
William-WakeTech <wakins@my.waketech.edu>
2025-04-11 23:00:04 -0400

    Add files via upload

---
+++ William Akins_gradebook.js
@@ -0,0 +1,4 @@
+function fetchGradeData(){console.log("Fetching grade data...");}
+function PopulateGradebook(data){console.log("Popluating gradebook with data:", data);}
+const gradeData = fetchGradeData();
+PopulateGradebook(gradedata);
