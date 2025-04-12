c7e73ee3d1dbbf783ec569e73ee425613cd1e684
William-WakeTech <wakins@my.waketech.edu>
2025-04-11 22:59:26 -0400

    Delete William Akins_gradebook.js

--- William Akins_gradebook.js
+++
@@ -1,4 +0,0 @@
-function fetchGradeData(){console.log("Fetching grade data...");}
-function PopulateGradebook(data){console.log("Popluating gradebook with data:", data);}
-const gradeData = fetchGradeData();
-PopulateGradebook(gradedata);
